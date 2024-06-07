ed13fcdcbcc3536abe84f122ea8b3e6768d49107
Vianam92 <workdeveloper.new@gmail.com>
2024-06-07 15:08:22 +0200

    feat: create register component

--- frontend/src/components/register/register.component.tsx
+++ frontend/src/components/register/register.component.tsx
@@ -1,3 +1,19 @@
+import { Button, Input } from "@mui/material";
+import "./register.component.styles.css";
+
 export const RegisterComponent = () => {
-    return <div>register</div>
+    return (
+        <form className="form-register">
+            <label htmlFor="username">UserName
+             <Input type="text" name="name" id="username" />
+            </label>
+            <label htmlFor="email"> Email
+                <Input type="email" name="email" id="email" />
+            </label>
+            <label htmlFor="password">Password
+            <Input type="password" name="email" id="password" />
+            </label>
+            <Button variant="outlined">Registrar</Button>
+        </form>
+    )
 }
